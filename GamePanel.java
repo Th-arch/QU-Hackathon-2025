04a707373fc271c2f28dc4db4d7ed2478a0d26bf
LionNub <liondoespk@gmail.com>
2025-11-08 10:15:49 -0500

    Add files via upload

---
+++ GamePanel.java
@@ -0,0 +1,64 @@
+
+/**
+ * Game Panel is the name of the JPanel we will
+ * be using to display things
+ 
+ */
+import javax.swing.*;
+import java.awt.Graphics;
+import java.awt.*;
+import javax.swing.JPanel;
+import java.io.*;
+import java.io.InputStream;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+public class GamePanel extends JPanel
+{
+    
+    private int xDelta = 0, yDelta = 0;
+    final int origionalTileSize = 32; // 32x32
+    final int scale = 3; // How big the character is
+    final int tileSize = origionalTileSize * scale; //96x96 tiles
+    final int maxScreenCol = 20;
+    final int maxScreenRow = 12;
+    final int ScreenWidth = tileSize * maxScreenCol; //1,920 pixels
+    final int ScreenHeight = tileSize * maxScreenRow; //1,152 pixels
+    private BufferedImage img;
+    public GamePanel() {
+        //this.setPreferredSize(new Dimension(ScreenWidth,ScreenHeight));
+        this.addKeyListener(new KeyboardInputs());
+        importImg();
+    }
+    // Used to draw things
+    public void paintComponent(Graphics g){ 
+        super.paintComponent(g);
+        g.fillRect(100 + xDelta,100 + yDelta,200,50);
+        g.drawImage(img, 0,0,null);
+    }
+    
+    public void importImg() {
+
+        try {
+            img = ImageIO.read(getClass().getResourceAsStream("/fish.png"));
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    public void changeXDelta(int value) {
+        this.xDelta += value;
+    }
+    
+    public void changeYDelta(int value) {
+        this.yDelta += value;
+    }
+    
+    public void setRectPos(int x, int y){
+        this.xDelta = x;
+        this.yDelta = y;
+    }
+    
+}
