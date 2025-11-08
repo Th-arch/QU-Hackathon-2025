1f0cb12c754812e2e8d0eb95ab3b5f612ee2f106
LionNub <liondoespk@gmail.com>
2025-11-08 10:15:11 -0500

    Implement basic Game class with window and panel

---
+++ Game.java
@@ -0,0 +1,14 @@
+import javax.swing.JPanel;
+
+public class Game
+{
+    private GameWindow gameWindow;
+    private GamePanel gamePanel;
+    public Game() {
+        gamePanel = new GamePanel();
+        gameWindow = new GameWindow(gamePanel);
+        gamePanel.setFocusable(true);
+        gamePanel.requestFocus();
+        
+    }
+}
